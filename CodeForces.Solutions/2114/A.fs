80fb2417ce765ea3ab61de2ee06750eceef611d0
Feroz Ahmmed <froghramar@gmail.com>
2025-06-10 16:34:45 +0600

    add solution 2114A

---
+++ CodeForces.Solutions/2114/A.fs
@@ -0,0 +1,17 @@
+﻿module CodeForces.Solutions.C2116.A
+
+open System
+
+let readInt () =
+    let input = Console.ReadLine()
+    input |> int
+
+let numberOfTests = readInt()
+for i in 1 .. numberOfTests do
+    let input = readInt()
+    
+    let b = input |> double |> sqrt |> int
+    if b * b = input then
+        Console.WriteLine $"0 {b}"
+    else
+        Console.WriteLine -1
